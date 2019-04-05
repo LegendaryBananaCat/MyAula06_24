6a203ea01ed1727d8c354de2431c0f0a19703b70
LegendaryBananaCat02 <34323618+LegendaryBananaCat@users.noreply.github.com>
2019-04-05 10:27:32 +0100

    Add files via upload

---
+++ test-solution-for-git/TestProjectForGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestProjectForGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I'm a test for git");
+        }
+    }
+}
